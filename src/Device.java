b42bd59761d610339c2d963c5ec820f2f70f7ebd
wujot <wojciech.wujot"gmail.com>
2018-01-17 21:53:28 +0100

    Class Device was created to represent features of objects. Two object constructors and method to represent information of each object in demanding fashion.

---
+++ src/Device.java
@@ -0,0 +1,22 @@
+public class Device {
+    String code;
+    String producer;
+    String type;
+    double price;
+
+    Device(String code, String producer, String type) {
+        this.code = code;
+        this.producer = producer;
+        this.type = type;
+    }
+
+    Device(String code, String producer, String type, double price) {
+        this(code, producer, type);
+        this.price = price;
+    }
+
+    String getInfo() {
+        return ("Kod: " + code + ", " + "producent: " + producer + ", " + "typ: " + type + ", " +
+        "cena: " + price);
+    }
+}
